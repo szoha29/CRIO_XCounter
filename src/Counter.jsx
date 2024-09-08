43dca9cc98bfe1824001de2b2bf29e126b399477
szoha2095 <szoha2095@gmail.com>
2024-09-08 17:15:46 +0530

    Counter App code changes

---
+++ src/Counter.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
